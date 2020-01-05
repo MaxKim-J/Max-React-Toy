7a2883fddcc8262b5830ff34d93ca830473789d6
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-01-05 17:46:16 +0900

    shouldComponentUpdate 메소드 이용하여 todoitemList 컴포넌트 최적화 삭제, 토글시 todos배열의 값이 달라질때만 렌더링하도록 수정

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+  // toggle되는 경우에도 리렌더링 될 필요 없음 -> checked 여부를 바인딩
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.checked !== nextProps.checked;
+  }
+
+  // 렌더 함수에 콘솔로그 집어넣어서 불필요한 타이밍에 렌더링되고 있는지 체크
   render() {
     const { text, checked, id, onToggle, onRemove } = this.props;
     return (
@@ -10,7 +17,7 @@ class TodoItem extends Component {
           e.stopPropagation();
           // 이벤트의 확산을 멈춰줌, 삭제부분에 들어간 이벤트 버블링이
           // 부모의 이벤트까지 전달되지 않도록 해줌
-          onRemove(id)
+          onRemove(id);
         }}>X
         </div>
         <div className={`todo-text ${checked ? ' checked' : ''}`}>
--- src/components/TodoItemList/TodoItemList.js
+++ src/components/TodoItemList/TodoItemList.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import TodoItem from '../TodoItem/TodoItem'
 // 클래스형 컴포 - 성능 최적화에 유용
 class TodoItemList extends Component {
+  // input prop이 바뀔때마다 렌더링이 되고 있는 상황 -> 굳이 필요 업따!
+  // 업데이트에 영향을 끼치는 조건을 리턴값으로 지정하면 todos가 바뀌면 렌더링함
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.todos !== nextProps.todos;
+  }
   render() {
     const { todos, onToggle, onRemove } = this.props;
     // map함수- 넘어온 todos배열을 컴포넌트 배열로 변환해준다
